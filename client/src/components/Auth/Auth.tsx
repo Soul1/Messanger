5063b7fc05cd9ac191bdf203271d37a0d9c522e5
Soul1 <darkwaud@gmail.com>
2020-04-26 15:32:00 +0300

    create directorys and files for ui

---
+++ client/src/components/Auth/Auth.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Register from "./Register/Register";
+import Login from "./Login/Login";
+
+const Auth = () => {
+  return (
+    <div>
+      <Login/>
+      <Register/>
+    </div>
+  );
+}
+
+export default Auth;
