d70b4e1bb7ef3179e956b18de61c57f100eea977
Soul1 <darkwaud@gmail.com>
2020-04-28 01:46:58 +0300

    made authorization with the help firebase

--- client/src/components/Auth/Login/Login.tsx
+++ client/src/components/Auth/Login/Login.tsx
@@ -1,22 +1,30 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
+import firebase from "firebase/app";
+
 
 const Login = () => {
-  const [form, setForm] = useState({email:'', password:''})
+  const [form, setForm] = useState({email: '', password: ''})
+  const onLoginClick = async () => {
+    try {
+      await firebase.auth().signInWithEmailAndPassword(form.email, form.password)
+    } catch (e) {
+
+    }
+  }
   const formHandler = (e: any) => setForm({...form, [e.target.name]: e.target.value})
   return (
     <div className='login container'>
       <div className="login__inner">
         <h3 className="login-title">Login</h3>
         <div className="login--form">
-          <form>
+          <div>
             <input type="email" value={form.email}
                    name="email" onChange={formHandler}/>
             <input type="password" value={form.password}
                    name="password" onChange={formHandler}/>
-            <button>Submit</button>
-          </form>
+            <button onClick={onLoginClick}>LogIn</button>
+          </div>
         </div>
-
       </div>
       <canvas id="canvas" width="400" height="400"/>
     </div>
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,12 +5,34 @@ import './index.scss';
 import App from './App';
 import {Provider} from "react-redux";
 import store from './redux/store';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App/>
-    </React.StrictMode>
-  </Provider>,
-  document.getElementById('root')
-);
+firebase.initializeApp({
+  apiKey: "AIzaSyBEpTkj56k0MrVgUH7arSLhUwhrCOGqzCg",
+  authDomain: "chat-c1043.firebaseapp.com",
+  databaseURL: "https://chat-c1043.firebaseio.com",
+  projectId: "chat-c1043",
+  storageBucket: "chat-c1043.appspot.com",
+  messagingSenderId: "123720687667",
+  appId: "1:123720687667:web:0d37ad0a71c4d737114346",
+  measurementId: "G-SZL4V4VERR"
+})
+
+let app: any;
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = ReactDOM.render(
+      <Provider store={store}>
+        <React.StrictMode>
+          <App/>
+        </React.StrictMode>
+      </Provider>,
+      document.getElementById('root')
+    );
+  }
+})
+
+
