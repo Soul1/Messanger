8c0870f79b827ce761043ad35fa9ae015a6e3acc
Soul1 <darkwaud@gmail.com>
2020-05-03 03:51:30 +0300

    Create Main.tsx

---
+++ client/src/components/Main/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Dialogs from "./Dialogs/Dialogs";
+import Body from "./Body/Body";
+
+const Main = () => {
+  return (
+    <>
+      <Dialogs/>
+      <Body/>
+    </>
+  );
+};
+
+export default Main;
