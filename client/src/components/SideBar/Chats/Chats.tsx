08d19663f16eb4733cf6f48437fc554c6e36a546
Soul1 <darkwaud@gmail.com>
2020-05-10 18:06:13 +0300

    Update Chats.tsx

--- client/src/components/SideBar/Chats/Chats.tsx
+++ client/src/components/SideBar/Chats/Chats.tsx
@@ -3,16 +3,20 @@ import {connect} from "react-redux";
 import {TChatsState, TChatState} from "../../../types/redux/reducers";
 import Chat from "./Chat/Chat";
 
-const Chats = ({catalogs}: {catalogs: TChatState}) => {
+interface IProps {
+  catalogs: TChatState[]
+}
+
+const Chats = ({catalogs}: IProps) => {
   return (
     <div>
       {catalogs.map(catalog => <Chat key={catalog.id} {...catalog}/>)}
     </div>
-  );
+  )
 }
 
-const mSTP = ({chats}: {chats: TChatsState}) => {
+const mSTP = ({chats}: { chats: TChatsState }) => {
   catalogs: chats.catalogs;
 }
 
-export default connect(mSTP)(Chats);
+export default connect(mSTP)(Chats)
