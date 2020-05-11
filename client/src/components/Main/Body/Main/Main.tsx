87586d5831d547ef01e9c85964054ac719a97d16
Soul1 <darkwaud@gmail.com>
2020-05-11 23:53:05 +0300

    add display messages, types for props and  connect(react-redux)

--- client/src/components/Main/Body/Main/Main.tsx
+++ client/src/components/Main/Body/Main/Main.tsx
@@ -1,17 +1,36 @@
 import React from 'react'
 import Message from "../../../common/Message/Message";
+import {connect} from "react-redux";
+import {TMessageState, TUserState} from "../../../../types/redux/reducers";
 
-const Main = () => {
+interface IProps {
+  messages: TMessageState[]
+  avatar?: string
+}
+
+const Main = (props: IProps) => {
   return (
     <main className='main'>
-      <div className='main__message'>
-        <div className='main__message-avatar'>
-          <img src='' alt='User Avatar'/>
-        </div>
-        <Message />
+      <div className='main__messages'>
+        {
+          props.messages.map((message: TMessageState) => <Message key={message.id}
+                                                                  avatar={props.avatar}
+                                                                  message={message}
+          />)
+        }
       </div>
     </main>
   )
 }
 
-export default Main;
+interface IMain {
+  messages: { messages: TMessageState[] }
+  user: TUserState
+}
+
+const mSTP = (state: IMain): IProps => ({
+  messages: state.messages.messages,
+  avatar: state.user.avatar
+})
+
+export default connect(mSTP)(Main);
