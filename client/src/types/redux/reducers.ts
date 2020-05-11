901cb3e8ae834bb231322a4dcd480bccfdd2f271
Soul1 <darkwaud@gmail.com>
2020-05-11 23:54:05 +0300

    update type TDialogsState

--- client/src/types/redux/reducers.ts
+++ client/src/types/redux/reducers.ts
@@ -1,35 +1,42 @@
+export type TUsersState = TUserState[]
+
 export type TUserState = {
-  id: number;
-  fullName: string;
-  status?: string;
-  isAuth: boolean;
+  id: number
+  fullName: string
+  status: string
+  isAuth: boolean
+  avatar?: string
 }
 
 export type TDialogState = {
-  id: number;
-  fullName: string;
-  lastMessage?: string;
-  avatar?: string;
+  id: number
+  fullName: string
+  lastMessage?: string
+  avatar?: string
 }
 
 export type TDialogsState = {
-  usersId?: number[];
+  dialogs?: TDialogState[]
 }
 
-export type TChatState = { id: number; catalog: string; }[];
+export type TChatState = {
+  id: number
+  catalog: string
+}
 
 export type TChatsState = {
-  catalogs?: TChatState;
+  catalogs?: TChatState[]
 }
 
 export type TThemeState = {
-  theme: string;
+  theme: string
 }
 
 export type TMessagesState = {
-  messages?: {
-    date: string;
-    message: string;
-    id: number;
-  }[]
+  messages?: TMessageState[]
+}
+export type TMessageState = {
+  date: string
+  message: string
+  id: number
 }
