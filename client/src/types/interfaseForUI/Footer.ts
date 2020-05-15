d30217af0655c3a9ea110a0d58392be98b1f6755
Soul1 <darkwaud@gmail.com>
2020-05-15 23:38:24 +0300

    update types

--- client/src/types/interfaseForUI/Footer.ts
+++ client/src/types/interfaseForUI/Footer.ts
@@ -1,3 +1,8 @@
-export interface FooterProps {
-  addMessage: (message: string, date: string) => void
+export type FooterProps = MSTP & MDTP
+
+export type MSTP = {
+}
+
+export type MDTP = {
+  addMessage: (message: string, date: string) => void;
 }
