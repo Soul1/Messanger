ac75525c5d49d9179ed10aea47de3e1212668b8b
Soul1 <darkwaud@gmail.com>
2020-05-17 23:54:08 +0300

    Update index.ts

--- client/src/redux/store/index.ts
+++ client/src/redux/store/index.ts
@@ -1,4 +1,4 @@
-import {combineReducers, createStore, compose} from "redux"
+import {combineReducers, createStore} from "redux"
 import Chats from '../reducers/chats'
 import Dialog from '../reducers/dialog'
 import Dialogs from '../reducers/dialogs'
@@ -19,14 +19,8 @@ const reducers = combineReducers({
 type rootReducers = typeof reducers
 export type appState = ReturnType<rootReducers>
 
-declare global {
-  interface Window {
-    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-  }
-}
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers());
+// @ts-ignore
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__());
 
 export default store
 
