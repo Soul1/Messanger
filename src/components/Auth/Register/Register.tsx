dd5ad5ced8f327e62971a302bddd81e1cc9d8e82
Soul1 <darkwaud@gmail.com>
2020-05-23 14:59:58 +0300

    Update Register.tsx

--- src/components/Auth/Register/Register.tsx
+++ src/components/Auth/Register/Register.tsx
@@ -1,52 +1,74 @@
 import React, {ChangeEvent, useEffect, useState} from 'react'
 import {connect} from 'react-redux'
-import {isAuthenticated} from '../../../redux/actios/user'
-import {appState} from "../../../redux/store";
-import {setError, setMessage} from "../../../redux/actios/globalMessage";
-import GlobalMessage from "../../../utils/GlobalMessage/GlobalMessage";
-import cn from "classnames";
-import api from "../../../utils/api/api";
+import {appState} from '../../../redux/store'
+import {setError, setMessage} from '../../../redux/actios/globalMessage'
+import GlobalMessage from '../../../utils/GlobalMessage/GlobalMessage'
+import cn from 'classnames'
+import api from '../../../utils/api/api'
 
 type IProps = MSTP & MDTP
 
-const Register = ({isAuthenticated, errMess, setMessage, setError}: IProps) => {
+const Register = ({errMess, setMessage, setError, userId}: IProps) => {
   const [form, setForm] = useState({email: '', password: '', nickname: ''})
   const [dis, setDis] = useState(false)
   const onRegisterClick = () => {
     try {
       setDis(true)
-      api.register(form.email, form.password, form.nickname)
+      api.register(form.email, form.password, userId)
       setDis(false)
-      isAuthenticated(true)
     } catch (e) {
       setDis(false)
-      setMessage('Ой, какая-то ошибка с данными... Попробуйте еще раз.')
       setError(true)
+      setMessage('Ой, какая-то ошибка с данными... Попробуйте еще раз.')
     }
   }
-  const formHandler = (e: ChangeEvent<HTMLInputElement>) => setForm({...form, [e.target.name]: e.target.value})
+  const formHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setForm({...form, [e.target.name]: e.target.value})
+  }
 
   useEffect(() => {
-    setError(false)
+    if (userId !== null) {
+      api.updateDatabaseAfterRegistered(userId, form.nickname)
+      setForm({email: '', password: '', nickname: ''})
+    }
+  }, [userId])
+
+  useEffect(() => {
+    if (errMess) {
+      setError(false)
+    }
   }, [form])
 
   return (
-    <div className='login width'>
-      <div className="login__inner">
-        <h3 className="login-title">Register</h3>
-        <div className="login--form">
-          <div>
-            <input type="text" value={form.nickname} required
-                   name="nickname" onChange={formHandler}/>
-            <input type="email" value={form.email} required
-                   name="email" onChange={formHandler}/>
-            <input type="password" value={form.password} required
-                   name="password" onChange={formHandler}/>
-            <button onClick={onRegisterClick} disabled={dis}>Register</button>
+    <div className='login'>
+      <div className="container">
+        <div className="login__inner">
+          <h3 className="login__title">Register</h3>
+          <div className="login--form">
+            <div>
+              <h4 className="login__designation">
+                Ваше Имя
+              </h4>
+              <input type="text" value={form.nickname} required
+                     name="nickname" onChange={formHandler}/>
+
+              <h4 className="login__designation">
+                Email
+              </h4>
+              <input type="email" value={form.email} required
+                     name="email" onChange={formHandler}/>
+
+              <h4 className="login__designation">
+                Пароль
+              </h4>
+              <input type="password" value={form.password} required
+                     name="password" onChange={formHandler}/>
+              <button onClick={onRegisterClick} disabled={dis}>Register</button>
+            </div>
           </div>
-        </div>
-        <div className={cn('login__message', {'display-none': !errMess})}>
-          <GlobalMessage/>
+          <div className={cn('login__message', {'display-none': !errMess})}>
+            <GlobalMessage/>
+          </div>
         </div>
       </div>
     </div>
@@ -54,16 +76,17 @@ const Register = ({isAuthenticated, errMess, setMessage, setError}: IProps) => {
 }
 
 type MDTP = {
-  isAuthenticated: (isAuth: boolean) => void
   setMessage: (message: string) => void
   setError: (err: boolean) => void
 }
 type MSTP = {
   errMess: boolean
+  userId: string
 }
 
 const mSTP = (state: appState): MSTP => ({
-  errMess: state.globalMessage.error
+  errMess: state.globalMessage.error,
+  userId: state.user.id
 })
 
-export default connect<MSTP, MDTP, {}, appState>(mSTP, {isAuthenticated, setMessage, setError})(Register);
+export default connect<MSTP, MDTP, {}, appState>(mSTP, {setMessage, setError})(Register);
