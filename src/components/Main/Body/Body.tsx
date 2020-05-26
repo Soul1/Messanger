d101905b3090888eb879d52d7aaa3270c46d22c6
Soul1 <darkwaud@gmail.com>
2020-05-26 04:47:15 +0300

    + class container

--- src/components/Main/Body/Body.tsx
+++ src/components/Main/Body/Body.tsx
@@ -2,21 +2,17 @@ import React from 'react'
 import Header from './Header/Header'
 import Main from './Main/Main'
 import Footer from './Footer/Footer'
-import {connect} from 'react-redux'
-import {appState} from "../../../redux/store";
 
 const Body = () => {
   return (
     <div className='body'>
-     <Header/>
-     <Main/>
-     <Footer/>
+      <div className="container">
+        <Header/>
+        <Main/>
+        <Footer/>
+      </div>
     </div>
   )
 }
 
-const mSTP = (state: appState) => {
-
-}
-
-export default connect() (Body)
+export default Body
