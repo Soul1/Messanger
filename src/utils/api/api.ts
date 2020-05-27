6316d8c771cf0595bac2c6cb46e9e13110cb671b
Soul1 <darkwaud@gmail.com>
2020-05-27 03:36:43 +0300

    add new methods: loadMessage and saveMessage (and no finished method savePhoto)

--- src/utils/api/api.ts
+++ src/utils/api/api.ts
@@ -1,12 +1,16 @@
-import firebase from 'firebase'
+import * as firebase from 'firebase/app'
 import {isAuthenticated, setToken, setUserId} from '../../redux/actios/user'
 import store from '../../redux/store'
 import jwt from 'jsonwebtoken'
+import {addMessage} from '../../redux/actios/messages'
 
 type TApi = {
   login: (email: string, password: string) => void
   register: (email: string, password: string, userId: string) => void
   currentUser: (userId: string) => void
+  savePhoto: (uid: string, photo: any) => void
+  loadMessages: (userId: string) => void
+  saveMessage: (userId: string, message: string, date: string) => void
   updateDatabaseAfterRegistered: (userId: string, name: string) => void
 }
 
@@ -38,7 +42,6 @@ const api: TApi = {
     } catch (e) {
       throw e
     }
-
   },
   updateDatabaseAfterRegistered: async (userId: string, name: string) => {
     try {
@@ -53,6 +56,61 @@ const api: TApi = {
     if (user && user.uid !== userId) {
       store.dispatch(setUserId(user?.uid))
     }
+  },
+  loadMessages: (userId) => {
+    const messagesRef = firebase.database().ref(`users/${userId}/messages`)
+    messagesRef.off()
+    const setMessage = (data: any) => {
+      const val = data.val()
+      store.dispatch(addMessage(val.text, val.date))
+    }
+    messagesRef.limitToLast(12)
+      .on('child_added', setMessage)
+    messagesRef.limitToLast(12)
+      .on("child_changed", setMessage)
+  },
+
+  saveMessage: (userId, message, date) => {
+    try {
+      if (message && !!firebase.auth().currentUser) {
+        const newPostKey = firebase.database().ref(`users/${userId}`).child('messages').push().key
+        firebase.database().ref(`users/${userId}/messages/${newPostKey}`).update({
+          text: message,
+          date,
+          id: newPostKey
+        })
+      }
+    } catch (e) {
+      console.error('Error writing new message to Firebase Database', e)
+    }
+  },
+
+  savePhoto: (userId, photo) => {
+    console.log(firebase.firestore().collection(`users/${userId}/messages`).id);
+    firebase.firestore().collection(`users/${userId}/messages`).add({
+      name: firebase.auth().currentUser?.displayName,
+      imageUrl: 'LOADING_IMAGE_URL',
+      profilePicUrl: firebase.auth().currentUser?.photoURL,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    }).then(function (messageRef) {
+      // Upload the image to Cloud Storage.
+      const filePath = `users/${userId}/messages/${messageRef.id}/${photo.name}`
+      return firebase.storage().ref(filePath).put(photo)
+        .then((fileSnapshot) => {
+          // Generate a public URL for the file.
+          return fileSnapshot.ref.getDownloadURL().then((url) => {
+            // Update the chat message placeholder with the image’s URL.
+            console.log(url);
+            console.log(fileSnapshot.metadata.fullPath);
+            return messageRef.update({
+              imageUrl: url,
+              storageUri: fileSnapshot.metadata.fullPath
+            });
+          });
+        });
+    }).catch(function (error) {
+      console.error('There was an error uploading a file to Cloud Storage:', error);
+    });
   }
 }
 
