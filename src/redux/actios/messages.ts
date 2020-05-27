4d58675f569f1d99f94d4de871acdb77c56f2fbe
Soul1 <darkwaud@gmail.com>
2020-05-27 03:27:59 +0300

    add new actions

--- src/redux/actios/messages.ts
+++ src/redux/actios/messages.ts
@@ -1,4 +1,16 @@
+import {TMessagesState} from '../../types/redux/reducers'
+
 export const addMessage = (message: string, date: string)
-  : {type: 'ADD_MESSAGE'; message: string, date: string} => {
-  return {type: 'ADD_MESSAGE', message, date,}
+  : {type: 'ADD_MESSAGE', message: string, date: string} => {
+  return {type: 'ADD_MESSAGE', message, date}
+}
+
+export const addImage = (url: string, date: string)
+  : {type: 'ADD_IMAGE', url: string, date: string} => {
+  return {type: 'ADD_IMAGE', url, date}
+}
+
+export const addMessages = (messages: TMessagesState)
+  : {type: 'ADD_MESSAGES', messages: TMessagesState} => {
+  return {type: 'ADD_MESSAGES', messages}
 }
