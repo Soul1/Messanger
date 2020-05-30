1d28f5c096ef782e2010fef15440c9895b82c592
Soul1 <darkwaud@gmail.com>
2020-05-30 18:52:48 +0300

    update markup, type and etc

--- src/components/Main/Dialogs/Dialog/Dialog.tsx
+++ src/components/Main/Dialogs/Dialog/Dialog.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import {connect} from "react-redux";
-import {appState} from "../../../../redux/store";
+import React from 'react'
 
-type TProps = MSTP
+type TProps = {
+  fullName: string
+  avatar: string
+  lastMessage?: string
+}
 
 const Dialog: React.FC<TProps> = (props) => {
   return (
@@ -10,22 +12,11 @@ const Dialog: React.FC<TProps> = (props) => {
       <div className="dialog__user-avatar avatar">
         <img src={props.avatar} alt="User Avatar"/>
       </div>
-      <div className="dialog__user-name"><b>{props.fullName}</b></div>
+      <div className="dialog__user-name">{props.fullName}</div>
       <div className="dialog__user-message">{props.lastMessage}</div>
     </div>
   );
 }
 
-type MSTP = {
-  fullName: string
-  avatar: string
-  lastMessage?: string
-}
 
-const mSTP = (state: appState): MSTP => ({
-  fullName: state.dialog.fullName,
-  avatar: state.dialog.avatar,
-  lastMessage: state.dialog?.lastMessage
-})
-
-export default connect<MSTP, {}, {}, appState>(mSTP)(Dialog);
+export default Dialog
--- src/components/Main/Dialogs/Dialogs.tsx
+++ src/components/Main/Dialogs/Dialogs.tsx
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, {useEffect} from 'react'
+import {appState} from '../../../redux/store'
+import {connect} from 'react-redux'
+import {TDialogsState} from '../../../types/redux/reducers'
+import Dialog from './Dialog/Dialog'
+import Search from './Search/Search'
+import api from '../../../utils/api/api'
+import {uuid} from 'uuidv4'
 
-const Dialogs = () => {
+type TProps = MSTP
+
+const Dialogs: React.FC<TProps> = ({dialogs, users}) => {
+
+  useEffect(() => {
+    const localUsers: any = localStorage.getItem('users')
+    if (users.length !== localUsers?.length) {
+      api.getUsers()
+    }
+  }, [])
+
   return (
-    <div>
-      {}
+    <div className='dialogs'>
+      <div className="dialogs-search">
+        <Search users={users}/>
+      </div>
+      <ul>
+        {dialogs?.dialogs?.map((d) =>
+          <li key={uuid()}>
+            <Dialog
+              fullName={d.fullName}
+              avatar={d.avatar}
+              lastMessage={d.lastMessage}
+            />
+          </li>
+        )}
+      </ul>
     </div>
   );
 }
 
-export default Dialogs;
+type MSTP = {
+  dialogs: TDialogsState
+  users: any
+}
+
+const mSTP = (state: appState): MSTP => ({
+  dialogs: state.dialogs,
+  users: state.users
+})
+
+export default connect<MSTP, {}, {}, appState>(mSTP)(Dialogs)
