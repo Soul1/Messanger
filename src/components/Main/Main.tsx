438630f69287cd5f441fab7093db69e0536005f9
Soul1 <darkwaud@gmail.com>
2020-05-30 18:51:15 +0300

    update markup

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Dialogs from "./Dialogs/Dialogs";
 import Body from "./Body/Body";
 
-const Main = () => {
+const Main: React.FC = () => {
   return (
-    <>
-      <Dialogs/>
-      <Body/>
-    </>
+    <div className='main-display'>
+      <div className="main-display__dialogs">
+        <Dialogs/>
+      </div>
+      <div className="main-display__body">
+        <Body/>
+      </div>
+    </div>
   );
 };
 
