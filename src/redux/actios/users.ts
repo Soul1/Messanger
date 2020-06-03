5e72e773246306e9430f26360844b1f396cd837d
Soul1 <darkwaud@gmail.com>
2020-06-03 03:49:28 +0300

    add thunk addUsersT

--- src/redux/actios/users.ts
+++ src/redux/actios/users.ts
@@ -1,5 +1,9 @@
-import {TUsersState} from "../../types/redux/reducers";
+import {TUsersState} from '../../types/redux/reducers'
 
 export const addUsers = (users: TUsersState): {type: 'ADD_USERS'; users: TUsersState} => {
   return {type: 'ADD_USERS', users}
+}
+
+export const addUsersT = (users: TUsersState) => async (dispatch: any) => {
+ await dispatch(addUsers(users))
 }
