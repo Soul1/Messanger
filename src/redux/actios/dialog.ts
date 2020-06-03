fae87d2a88bdb1710205e2df618f17ef48349568
Soul1 <darkwaud@gmail.com>
2020-06-03 03:49:56 +0300

    add new action setLastMessage

--- src/redux/actios/dialog.ts
+++ src/redux/actios/dialog.ts
@@ -1,3 +1,6 @@
 export const setId = (id: string): { type: 'SET_ID', id: string } => {
   return{ type: 'SET_ID', id}
 }
+export const setLastMessage = (message: string): { type: 'SET_LAST_MESSAGE', message: string } => {
+  return{ type: 'SET_LAST_MESSAGE', message}
+}
