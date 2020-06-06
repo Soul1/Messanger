173d6bed0b54b2537d76f5ad59318c71daae41af
Soul1 <darkwaud@gmail.com>
2020-06-06 19:39:17 +0300

    made display component Dialogs if is users

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,18 +1,33 @@
-import React from 'react';
-import Dialogs from "./Dialogs/Dialogs";
-import Body from "./Body/Body";
+import React from 'react'
+import Dialogs from './Dialogs/Dialogs'
+import Body from './Body/Body'
+import {connect} from 'react-redux'
+import {appState} from '../../redux/store'
+import {TUsersState} from '../../types/redux/reducers'
 
-const Main: React.FC = () => {
+type TProps = MSTP
+
+const Main: React.FC<TProps> = ({users}) => {
+
   return (
     <div className='main-display'>
+
       <div className="main-display__dialogs">
-        <Dialogs/>
+        {!!users?.length && <Dialogs/>}
       </div>
       <div className="main-display__body">
         <Body/>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Main;
+type MSTP = {
+  users: TUsersState
+}
+
+const mSTP = (state: appState): MSTP => ({
+  users: state.users
+})
+
+export default connect<MSTP, {}, {}, appState>(mSTP)(Main)
