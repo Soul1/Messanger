afc037491e641221086d0de84a85b4c02e9dde13
Soul1 <darkwaud@gmail.com>
2020-06-06 19:35:37 +0300

    add api setNewRoom and made fnc onDisplayThisRoom

--- src/components/Main/Dialogs/Dialog/Dialog.tsx
+++ src/components/Main/Dialogs/Dialog/Dialog.tsx
@@ -1,21 +1,29 @@
 import React from 'react'
+import api from '../../../../utils/api/api'
 
 type TProps = {
   fullName: string
   avatar: string
   lastMessage?: string
+  id: string
+  uid: string
 }
 
 const Dialog: React.FC<TProps> = (props) => {
+
+  const onDisplayThisRoom = () => {
+    api.setNewRoom(props.uid, props.id)
+  }
+
   return (
-    <div className='dialog'>
+    <div className='dialog' onClick={onDisplayThisRoom}>
       <div className="dialog__user-avatar avatar">
         <img src={props.avatar} alt="User Avatar"/>
       </div>
       <div className="dialog__user-name">{props.fullName}</div>
       <div className="dialog__user-message">{props.lastMessage}</div>
     </div>
-  );
+  )
 }
 
 
