b98e33af2f885c08abd3ff6aacfbf6a020702ec9
Soul1 <darkwaud@gmail.com>
2020-06-06 19:38:02 +0300

    add action setId

--- src/components/Main/Dialogs/Search/SearchList/SearchList.tsx
+++ src/components/Main/Dialogs/Search/SearchList/SearchList.tsx
@@ -3,13 +3,15 @@ import {TList} from "../Search";
 import api from "../../../../../utils/api/api";
 import {appState} from "../../../../../redux/store";
 import {connect} from "react-redux";
+import {setId} from "../../../../../redux/actios/dialog";
 
-type TProps = TList & MSTP
+type TProps = TList & MSTP & MDTP
 
-const SearchList: React.FC<TProps> = ({name, id, uid}) => {
+const SearchList: React.FC<TProps> = ({name, id, uid, setId}) => {
 
   const onSetNewRoom = () => {
-   api.setNewRoom(uid, id)
+    api.setNewRoom(uid, id)
+    setId(id)
   }
 
   return (
@@ -24,8 +26,11 @@ const SearchList: React.FC<TProps> = ({name, id, uid}) => {
 type MSTP = {
   uid: string
 }
+type MDTP = {
+  setId: (id: string) => void
+}
 
 const mSTP = (state: appState): MSTP => ({
   uid: state.user.id
 })
-export default connect<MSTP, {}, {} , appState>(mSTP) (SearchList)
+export default connect<MSTP, MDTP, {}, appState>(mSTP, {setId})(SearchList)
